e54eb47ae85a5d0a0e61fcc00ae2fcbd8cfc167b
Vasanth Ammaiappan <avasant21@gmail.com>
2019-12-14 00:23:15 +0530

    Updated Java comment

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,5 @@
 public class helloworld {
   public static void main(String[] args){
-    System.out.println("Project: Hello World :) ");
+    System.out.println("Project run by Milindtech: Hello World :) ");
   }
 }
