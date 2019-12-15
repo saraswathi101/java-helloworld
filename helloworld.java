cbdc5ad81ebdf3facbc28e82c3338abf20c1c997
saraswathi101 <56471402+saraswathi101@users.noreply.github.com>
2019-12-15 14:41:54 +0530

    updated print statement

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,5 @@
 public class helloworld {
   public static void main(String[] args){
-    System.out.println("Project run by Milind Technology: Hello World :) ");
+    System.out.println("Project run by Milind Technology: Hello World Program:) ");
   }
 }
